d3c1c600c51b3556273e5231b8d162f02a546bd7
Cam <cjryanwashere@gmail.com>
2023-05-11 20:03:41 -0400

    Initial Commit

---
+++ Kinetic App/Kinetic_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Kinetic_AppApp.swift
+//  Kinetic App
+//
+//  Created by CJ Ryan on 5/11/23.
+//
+
+import SwiftUI
+
+@main
+struct Kinetic_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Kinetic AppUITests/Kinetic_AppUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  Kinetic_AppUITestsLaunchTests.swift
+//  Kinetic AppUITests
+//
+//  Created by CJ Ryan on 5/11/23.
+//
+
+import XCTest
+
+final class Kinetic_AppUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
